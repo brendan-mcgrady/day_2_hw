ce69b28a05b1cc3504df848ff9a3416cc326e1fa
Brendan <laotsu@hotmail.co.uk>
2019-11-05 15:58:45 +0000

    Error method add fixed

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,10 +1,14 @@
-# def test_return_10()
-#   return_10_result = return_10()
-#   assert_equal( 10, return_10_result )
-# end
+def test_add()
+  add_result = add( 1, 2 )
+  assert_equal( 3, add_result )
+end
 
 def return_10()
   return_10 = 10
+end
+
+def add()
+  1 + 2 = 3
 end
 
 
