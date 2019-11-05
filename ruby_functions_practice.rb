690e191ec780350d52c96c1dcf2c19469e32493e
Brendan <laotsu@hotmail.co.uk>
2019-11-05 15:53:10 +0000

    First error, return_10 fixed

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,22 @@
+# def test_return_10()
+#   return_10_result = return_10()
+#   assert_equal( 10, return_10_result )
+# end
+
+def return_10()
+  return_10 = 10
+end
+
+
+# begin def greet(name, time_of_day)
+#   return "Good #{time_of_day}, #{name.capitalize()}"
+# end
+#
+# p greet("anna", "afternoon")
+#
+# def add(first_number, second_number)
+#   return first_number + second_number
+# end
+#
+# p add(6, 3)
+# end
